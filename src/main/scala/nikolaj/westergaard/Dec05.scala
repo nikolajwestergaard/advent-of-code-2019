d9369cb8ed314d9cf23a5673ddd3f5891677f2fe
Nikolaj Westergaard <nikolaj.westergaard@dnb.no>
2019-12-05 13:48:29 +0100

    Dec 05 - Part 01 - Refactor

--- src/main/scala/nikolaj/westergaard/Dec05.scala
+++ src/main/scala/nikolaj/westergaard/Dec05.scala
@@ -6,35 +6,23 @@ import scala.util.{Success, Try}
 object Dec05 {
   val mem: mutable.HashMap[Int, Int] = mutable.HashMap()
 
-  def process(ins: List[Int], testinst: List[Int], pos: Int = 0): List[Int] = ins(pos) match {
+  def process(ins: List[Int], testinst: List[Int], pos: Int = 0, m1: Int = 0, m2: Int = 0): List[Int] = ins(pos) match {
     case p if p > 99 =>
       val s = p.abs.formatted("%05d")
-      s.substring(3) match {
+      val m1 = s(2).asDigit
+      val m2 = s(1).asDigit
+      val op = s.substring(3).toInt
+      process(ins.updated(pos, op), testinst, pos, m1, m2)
 
-        case "01" =>
-          val v1 = if (s(2) == '0') ins(ins(pos + 1)) else ins(pos + 1)
-          val v2 = if (s(1) == '0') ins(ins(pos + 2)) else ins(pos + 2)
-          process(ins.updated(ins(pos + 3), v1 + v2), testinst, pos + 4)
-
-        case "02" =>
-          val v1 = if (s(2) == '0') ins(ins(pos + 1)) else ins(pos + 1)
-          val v2 = if (s(1) == '0') ins(ins(pos + 2)) else ins(pos + 2)
-          process(ins.updated(ins(pos + 3), v1 * v2), testinst, pos + 4)
-
-        case "04" =>
-          println(f"out: ${ins(pos + 1)}")
-          process(ins, testinst, pos + 2)
-
-        case err =>
-          println(s"err ${err}")
-          process(ins, testinst, pos + 1)
-
-      }
     case 1 =>
-      process(ins.updated(ins(pos + 3), ins(ins(pos + 1)) + ins(ins(pos + 2))), testinst, pos + 4)
+      val v1 = if (m1 == 0) ins(ins(pos + 1)) else ins(pos + 1)
+      val v2 = if (m2 == 0) ins(ins(pos + 2)) else ins(pos + 2)
+      process(ins.updated(ins(pos + 3),  v1 + v2), testinst, pos + 4)
 
     case 2 =>
-      process(ins.updated(ins(pos + 3), ins(ins(pos + 1)) * ins(ins(pos + 2))), testinst, pos + 4)
+      val v1 = if (m1 == 0) ins(ins(pos + 1)) else ins(pos + 1)
+      val v2 = if (m2 == 0) ins(ins(pos + 2)) else ins(pos + 2)
+      process(ins.updated(ins(pos + 3),  v1 * v2), testinst, pos + 4)
 
     case 3 =>
       process(ins.updated(ins(pos + 1), testinst.head), testinst.drop(1), pos + 2)
